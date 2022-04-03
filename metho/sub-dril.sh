3ea7bb39480f211264d3711fa7c681f71025f2aa
Mr-koanti <82882556+Mr-koanti@users.noreply.github.com>
2022-04-03 01:17:43 +0000

    Update sub-dril.sh

--- metho/sub-dril.sh
+++ metho/sub-dril.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#while read domain; do 
+#'/home/mrk/Desktop/bugbounty/methodolgy/my scripts/Sub-Drill.sh' $domain | anew subdomain/$domain-sub.txt
+#done < $1
+mkdir subdomain; mkdir alive ; mkdir alive/scode 
 while read domain; do 
-'/home/mrk/Desktop/bugbounty/methodolgy/my scripts/Sub-Drill.sh' $domain | sort -u  | httpx -sc -cl -ports 80,8080,8448,433,21,445,3306,1433,3389,22 | anew alive/scode/$domain-statecode.txt | cut -d "[" -f 1 | anew alive/$domain-livsub.txt
-done < $1
+'/home/mrk/Desktop/bugbounty/methodolgy/my scripts/Sub-Drill.sh' $domain | sort -u  | anew subdomain/$domain.txt;
+cat subdomain/$domain.txt |httpx -sc -cl -title -td -server -ports 80,8080,8448,433,21,445,3306,1433,3389,22   | anew alive/scode/$domain-statecode.txt | cut -d "[" -f 1 | anew alive/$domain-livsub.txt ;
+
+done < $1 
+
